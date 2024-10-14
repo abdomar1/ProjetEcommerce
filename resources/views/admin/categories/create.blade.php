529605b207db995a0b99caf508fb8da0825d5a3e
abdessmad7 <hhhanay8@gmail.com>
2024-10-14 14:24:10 +0100

    mise ajour

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -18,22 +18,22 @@
         @csrf
 
         <div class="form-group">
-            <label for="nom">اسم الفئة</label>
+            <label for="nom">nom categorie</label>
             <input type="text" name="nom" id="nom" class="form-control" required>
         </div>
 
         <div class="form-group">
-            <label for="description">وصف الفئة (اختياري)</label>
+            <label for="description">description</label>
             <textarea name="description" id="description" class="form-control"></textarea>
         </div>
 
         <div class="form-group">
-            <label for="image">صورة الفئة (اختياري)</label>
+            <label for="image">image</label>
             <input type="file" name="image" id="image" class="form-control" accept="image/*">
         </div>
         
 
-        <button type="submit" class="btn btn-primary">إضافة فئة</button>
+        <button type="submit" class="btn btn-primary">Ajoute</button>
     </form>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\CommandeDetailController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\HomeController;
 use App\Mail\ContacteMail;
-
+use Illuminate\Support\Facades\Mail;
 
 /////////////////////////////////////
 
